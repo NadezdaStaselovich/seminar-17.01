2d11e4ff2c980e87a6f91fbf76c9f9c45e0e3c0d
nadezda staselovich <staselovichn@mail.ru>
2023-01-22 15:01:29 +0200

    add commit

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = (num % 100) / 10;
+System.Console.WriteLine($"{num} -> {num2}");
---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (num >= 100)
+{
+  result = num % 10;
+  System.Console.WriteLine(result);
+}
+else
+{
+  System.Console.WriteLine("третьей цифры нет");
+}
